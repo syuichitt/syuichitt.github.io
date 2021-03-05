968376e2e63016fcad5cfa18cdd4913bee753752
syuichitt <48198180+syuichitt@users.noreply.github.com>
2021-03-06 06:11:22 +0800

    测试

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.06482e26265ec96cdeed54b648f04c76.js"
+  "./precache-manifest.88a3e322b48dd3300c23173737485239.js"
 );
 
 self.addEventListener('message', (event) => {
